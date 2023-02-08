70345f76a78272695c0b05ea6bf6c2fd85154561
haranrk <haranrajkumar97@gmail.com>
2023-02-08 14:24:30 -0500

    fix: example.py

--- example.py
+++ example.py
@@ -1,17 +1,20 @@
 # Sample app to test the Slack microservice
-import requests
 import json
 import os
+import requests
+from wayscript.utils import get_application_key
 
 WAYSCRIPT_LAIR_URL = os.getenv('WAYSCRIPT_LAIR_URL')
+session = requests.Session()
+session.headers["authorization"] = f"Bearer {get_application_key()}"
 
-r = requests.get(f'{WAYSCRIPT_LAIR_URL}/status')
-print(r.text)
 
-r = requests.get(f'{WAYSCRIPT_LAIR_URL}/channels')
-print(r.text)
+status_resp = session.get(f'{WAYSCRIPT_LAIR_URL}/status')
+print(status_resp.text)
 
-channels = json.loads(r.text)
+channels_resp = session.get(f'{WAYSCRIPT_LAIR_URL}/channels')
+channels = json.loads(channels_resp.text)
+
 print(f"The message will be sent to {channels[0][1]}. Proceed (Y/n)?")
 choice = input()
 if choice == "Y":
@@ -20,7 +23,7 @@ if choice == "Y":
         "message": "Hello :tada:"
     }
 
-    r = requests.post(f"{WAYSCRIPT_LAIR_URL}/message", json=data)
-    print(r.text)
+    message_post_resp = session.post(f"{WAYSCRIPT_LAIR_URL}/message", json=data)
+    print(message_post_resp.text)
 else:
     print("Message not sent")
