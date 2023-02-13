f1c07821c6f7c55e1d798837c96fdf7860f68eec
haranrk <haranrajkumar97@gmail.com>
2023-02-13 13:26:58 -0500

    add black autoformatter

--- example.py
+++ example.py
@@ -4,24 +4,21 @@ import os
 import requests
 from wayscript.utils import get_application_key
 
-WAYSCRIPT_LAIR_URL = os.getenv('WAYSCRIPT_LAIR_URL')
+WAYSCRIPT_LAIR_URL = os.getenv("WAYSCRIPT_LAIR_URL")
 session = requests.Session()
 session.headers["authorization"] = f"Bearer {get_application_key()}"
 
 
-status_resp = session.get(f'{WAYSCRIPT_LAIR_URL}/status')
+status_resp = session.get(f"{WAYSCRIPT_LAIR_URL}/status")
 print(status_resp.text)
 
-channels_resp = session.get(f'{WAYSCRIPT_LAIR_URL}/channels')
+channels_resp = session.get(f"{WAYSCRIPT_LAIR_URL}/channels")
 channels = json.loads(channels_resp.text)
 
 print(f"The message will be sent to {channels[0][1]}. Proceed (Y/n)?")
 choice = input()
 if choice == "Y":
-    data = {
-        "target_id": channels[0][0],
-        "message": "Hello :tada:"
-    }
+    data = {"target_id": channels[0][0], "message": "Hello :tada:"}
 
     message_post_resp = session.post(f"{WAYSCRIPT_LAIR_URL}/message", json=data)
     print(message_post_resp.text)
--- tests/test_managers.py
+++ tests/test_managers.py
@@ -1,5 +1,5 @@
 import pytest
-from unittest.mock import  Mock
+from unittest.mock import Mock
 from slack_sdk.errors import SlackApiError
 from managers.slack_manager import SlackManager, SlackManagerError
 
@@ -7,24 +7,29 @@ from managers.slack_manager import SlackManager, SlackManagerError
 def test_slack_manager():
     slack_manager = SlackManager()
     slack_manager.client = Mock()
-    slack_manager.client.conversations_list.return_value = {"channels": [
-        {"id": "C2312", "name": "random ", "other data": "lorem ipsum"},
-        {"id": "C5312", "name": "general "}
-    ]}
-    assert slack_manager.get_channel_list() == [(
-        'C2312', 'random '), ('C5312', 'general ')]
+    slack_manager.client.conversations_list.return_value = {
+        "channels": [
+            {"id": "C2312", "name": "random ", "other data": "lorem ipsum"},
+            {"id": "C5312", "name": "general "},
+        ]
+    }
+    assert slack_manager.get_channel_list() == [
+        ("C2312", "random "),
+        ("C5312", "general "),
+    ]
 
     with pytest.raises(SlackManagerError) as e_info:
         slack_manager.client.conversations_list.side_effect = SlackApiError(
-            "slack exception", {"error": 'not_authed'})
+            "slack exception", {"error": "not_authed"}
+        )
         slack_manager.get_channel_list()
 
-    slack_manager.client.chat_postMessage.return_value = ''
+    slack_manager.client.chat_postMessage.return_value = ""
     slack_manager.send_message("test_id", "test_message")
     slack_manager.client.chat_postMessage.assert_called_once()
 
     with pytest.raises(SlackManagerError) as e_info:
         slack_manager.client.chat_postMessage.side_effect = SlackApiError(
-            "slack exception", {"error": 'not_authed'})
+            "slack exception", {"error": "not_authed"}
+        )
         slack_manager.send_message("test_id", "test_message")
-
