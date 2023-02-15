8fe1b2a48334e1980940f566d42346d4890e31f8
haranrk <haranrajkumar97@gmail.com>
2023-02-16 01:16:04 +0530

    deepen paramaterization use

--- tests/test_client.py
+++ tests/test_client.py
@@ -10,7 +10,17 @@ def app():
     yield app
 
 
-def test_get_channels_status(app):
+@pytest.mark.parametrize(
+    " error, expected_response",
+    [
+        ("", {"status": "Slack API is connected and working properly"}),
+        (
+            SlackManagerError("test_error"),
+            {"error": "Slack exception ocurred: test_error"},
+        ),
+    ],
+)
+def test_get_status(app, error, expected_response):
     slack_manager = Mock()
     app.config["SlackManager"] = slack_manager
     sample_channels = [
@@ -19,59 +29,87 @@ def test_get_channels_status(app):
         ["CEQ4SQQAF", "blog_posts"],
     ]
     slack_manager.get_channel_list.return_value = sample_channels
-    response = app.test_client().get("/channels")
-    assert json.loads(response.data.decode()) == sample_channels
+    if error:
+        slack_manager.get_channel_list.side_effect = error
 
     response = app.test_client().get("/status")
-    assert json.loads(response.data.decode()) == {
-        "status": "Slack API is connected and working properly"
-    }
 
-    slack_manager.get_channel_list.side_effect = SlackManagerError("test_error")
-    response = app.test_client().get("/channels")
-    assert json.loads(response.data.decode()) == {
-        "error": "Slack exception ocurred: test_error"
-    }
-
-    response = app.test_client().get("/status")
-    assert json.loads(response.data.decode()) == {
-        "error": "Slack exception ocurred: test_error"
-    }
-
-
-successful_message_sent_response = {"message": "Message successfully posted to channel"}
-incorrect_params_response = {"error": "Required parameters not present"}
+    assert json.loads(response.data.decode()) == expected_response
 
 
 @pytest.mark.parametrize(
-    "params, expected_response",
+    "channels, error, expected_response",
+    [
+        (
+            [
+                ["CB8886H7V", "random"],
+                ["CB98179M4", "general"],
+                ["CEQ4SQQAF", "blog_posts"],
+            ],
+            "",
+            [
+                ["CB8886H7V", "random"],
+                ["CB98179M4", "general"],
+                ["CEQ4SQQAF", "blog_posts"],
+            ],
+        ),
+        (
+            [
+                ["CB8886H7V", "random"],
+                ["CB98179M4", "general"],
+                ["CEQ4SQQAF", "blog_posts"],
+            ],
+            SlackManagerError("test_error"),
+            {"error": "Slack exception ocurred: test_error"},
+        ),
+    ],
+)
+def test_get_channels(app, channels, error, expected_response):
+    slack_manager = Mock()
+    app.config["SlackManager"] = slack_manager
+    slack_manager.get_channel_list.return_value = channels
+    if error:
+        slack_manager.get_channel_list.side_effect = error
+
+    response = app.test_client().get("/channels")
+
+    assert json.loads(response.data.decode()) == expected_response
+
+
+@pytest.mark.parametrize(
+    "params, error, expected_response",
     [
         (
             {"message": "hello world", "target_id": "CXXXXX"},
-            successful_message_sent_response,
+            "",
+            {"message": "Message successfully posted to channel"},
         ),
-        ({"message": "test message"}, incorrect_params_response),
+        (
+            {"message": "hello world", "target_id": "CXXXXX"},
+            SlackManagerError("test_error"),
+            {"error": "Slack exception ocurred: test_error"},
+        ),
+        (
+            {"message": "test message"},
+            "",
+            {"error": "Required parameters not present"},
+        ),
+        (
+            {"message": "test message"},
+            SlackManagerError("test_error"),
+            {"error": "Required parameters not present"},
+        ),
     ],
 )
-def test_post_message(app, params, expected_response):
+def test_post_message(app, params, error, expected_response):
     slack_manager = Mock()
     app.config["SlackManager"] = slack_manager
-    slack_manager.send_message.return_value = successful_message_sent_response
-    response = app.test_client().post("/message", json=params)
-    assert json.loads(response.data.decode()) == expected_response
-
-    # response = app.test_client().post(
-    #     "/message",
-    #     json={
-    #         "message": "Hello world",
-    #     },
-    # )
-    # assert json.loads(response.data.decode()) == incorrect_params_response
-
-    slack_manager.send_message.side_effect = SlackManagerError("test_error")
-    response = app.test_client().post(
-        "/message", json={"message": "Hello world", "target_id": "CXXXX"}
-    )
-    assert json.loads(response.data.decode()) == {
-        "error": "Slack exception ocurred: test_error"
+    slack_manager.send_message.return_value = {
+        "message": "Message successfully posted to channel"
     }
+    if error:
+        slack_manager.send_message.side_effect = SlackManagerError("test_error")
+
+    response = app.test_client().post("/message", json=params)
+
+    assert json.loads(response.data.decode()) == expected_response
